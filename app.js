3ac52a5fc7f03ad2278748eff601464527a8141f
lqwangxg@gmail.com <U0Bd9Oa4>
2020-05-22 18:40:15 +0900

    update request body of fulfillmenttext

--- app.js
+++ app.js
@@ -48,7 +48,11 @@ server.post('/movie', function(req, res){
           const movie = JSON.parse(completeResponse);
           let dataToSend = movieTitle === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
           dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-
+          req.body.queryResult.fulfillmentText = dataToSend;
+          req.body.queryResult.fulfillmentMessages.text.text = dataToSend;
+          
+          console.log(`💫💫💫call webhook succeed.💫💫💫`);
+          
           return res.json({
               speech: dataToSend,
               displayText: dataToSend,
